0478d2c39fc3bfd72db5042eb9f43f185f6ea162
ovaisophy <68985203+ovaisophy@users.noreply.github.com>
2024-09-15 13:15:33 +0300

    first code

---
+++ lab3test3.cs
@@ -0,0 +1,20 @@
+using System;
+public class A
+{
+    public static void Main()
+    {
+        int num1, num2, num3, num4, num5;
+        num1 = 5;
+        num2 = 3;
+        num3 = A.strange(num1, num2);
+        num4 = A.strange(num3, num1);
+        num5 = A.strange(num1, num3);
+        string str = "num3= " + num3 + " and num4= " + num4;
+        Console.Write(str);
+    }
+
+    public static int strange(int a, int y)
+    {
+        return y + a;
+    }
+}
